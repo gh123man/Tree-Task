5a88ff836d14adc4f1fdea4669766ba0eb01a41d
Brian Floersch <gh123man@gmail.com>
2013-07-13 15:11:52 -0400

    values updateable now

--- src/com/ghsoft/treetask/TaskView.java
+++ src/com/ghsoft/treetask/TaskView.java
@@ -16,6 +16,7 @@ import com.example.treetask.R;
 public class TaskView extends Activity {
 
 	ListView taskList;
+	TaskNode task;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,8 +24,7 @@ public class TaskView extends Activity {
 		
 		Object sTask = getIntent().getSerializableExtra("task");
 		
-		final TaskNode task = (TaskNode)sTask;
-		
+		task = (TaskNode)sTask;
 		
 		View header = getLayoutInflater().inflate(R.layout.header, null); 
 		
@@ -41,29 +41,29 @@ public class TaskView extends Activity {
 		
 		taskList.addHeaderView(header); 
 		
-		TaskViewListItem adapter = new TaskViewListItem(this, getApplicationContext(), task);
+		TaskViewListItem adapter = new TaskViewListItem(this, getApplicationContext(), task, header);
 
 		taskList.setAdapter(adapter);
 		
 		
+	}
+	
+	
+	public void refreshHeader(TaskNode task) {
 		
-		taskList.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> arg0, View arg1,
-					int position, long arg3) {
-				position--;
-				
-				if (task.getChildren().get(position).hasChildren()) {
-					Intent i = new Intent(TaskView.this, TaskView.class);
-					i.putExtra("task", task.getChildren().get(position));
-					startActivity(i);
-				} else {
-					TaskLeaf t = (TaskLeaf)task.getChildren().get(position);
-					t.setFinished(!t.getFinished());
-					Log.d("count", Integer.toString(t.completion()));
-				}
-				
-			}
-		});
+		this.task = task;
+		
+		Log.d("complt", Integer.toString(task.completion()));
+		
+		View header = getLayoutInflater().inflate(R.layout.header, null); 
+		
+		TextView name = (TextView) header.findViewById(R.id.hname);
+		TextView description = (TextView) header.findViewById(R.id.hdescription);
+		TextView completion = (TextView) header.findViewById(R.id.hcompletion);
+		
+		name.setText(task.getName());
+		description.setText(task.getDescription());
+		completion.setText(Integer.toString(task.completion()));
 	}
 
 }
--- src/com/ghsoft/treetask/TaskViewListItem.java
+++ src/com/ghsoft/treetask/TaskViewListItem.java
@@ -4,6 +4,8 @@ import com.example.treetask.R;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,12 +18,16 @@ public class TaskViewListItem extends BaseAdapter {
 	Activity act;
 	TextView name, description, completion;
 	TaskNode task;
+	View header;
 
-	public TaskViewListItem(Activity act, Context context, TaskNode task) {
+	public TaskViewListItem(Activity act, Context context, TaskNode task, View header) {
 		// Caches the LayoutInflater for quicker use
 		this.inflater = LayoutInflater.from(context);
 		// Sets the events data
 		this.task = task;
+		this.act = act;
+		this.header = header;
+		this.context = context;
 	}
 
 
@@ -45,12 +51,13 @@ public class TaskViewListItem extends BaseAdapter {
 		return task.numChildren();
 	}
 
-	public View getView(int position, View convertView, ViewGroup parent) {
+	public View getView(final int position, View convertView, ViewGroup parent) {
 		
-		Task t = task.getChildren().get(position);
+		
+		final Task t = task.getChildren().get(position);
 		
 		convertView = this.inflater.inflate(R.layout.taskviewlistitem, null);
-
+		
 		name = (TextView) convertView.findViewById(R.id.name);
 		description = (TextView) convertView.findViewById(R.id.description);
 		completion = (TextView) convertView.findViewById(R.id.completion);
@@ -58,6 +65,40 @@ public class TaskViewListItem extends BaseAdapter {
 		name.setText(t.getName());
 		description.setText(t.getDescription());
 		completion.setText(Integer.toString(t.completion()));
+		
+		
+		convertView.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Log.d("count", Integer.toString(position));
+				
+				if (t.hasChildren()) {
+					
+					Intent i = new Intent(context, TaskView.class);
+					i.putExtra("task", t);
+					act.startActivity(i);
+					
+				} else {
+					TaskLeaf tl = (TaskLeaf)t;
+					tl.setFinished(!tl.getFinished());
+					
+					completion = (TextView) v.findViewById(R.id.completion);
+					
+					completion.setText(Integer.toString(t.completion()));
+					
+					TaskViewListItem.this.notifyDataSetChanged();
+					
+					TextView completion = (TextView) header.findViewById(R.id.hcompletion);
+					completion.setText(Integer.toString(task.completion()));
+					
+					TaskView tv = (TaskView)act;
+					
+					//Log.d("count", Integer.toString(t.completion()));
+				}
+			}
+		});
 
 		return convertView;
 	}
