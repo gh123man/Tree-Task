6dee5c551d71c1ce40dc291c4c9bf1df9769e6e5
Brian Floersch <gh123man@gmail.com>
2013-07-12 20:07:39 -0400

    initial commit

---
+++ src/com/example/treetask/MainViewFragment.java
@@ -0,0 +1,34 @@
+package com.example.treetask;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MainViewFragment extends Fragment {
+	
+	
+	/**
+	 * The fragment argument representing the section number for this
+	 * fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	public MainViewFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_main_dummy,
+				container, false);
+		TextView dummyTextView = (TextView) rootView
+				.findViewById(R.id.section_label);
+		dummyTextView.setText(Integer.toString(getArguments().getInt(
+				ARG_SECTION_NUMBER)));
+		return rootView;
+	}
+
+}
