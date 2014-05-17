d26ba0dcda318aa94e6a8e0298b5498312dfe55a
VladimirStrelok <alexander.glica@gmail.com>
2014-05-17 10:50:39 -0400

    added sections and navbar

--- js/global.js
+++ js/global.js
@@ -1,9 +1,25 @@
+var $nav, $window, $header;
 $( document ).ready(function() {
-    console.log('ready');
+    $nav = $('#nav');
+    $window = $(window);
+    $content = $("#content");
+
+    setTimeout(function() {
+        if ($window.scrollTop() < 1) {
+            $window.scrollTo(20, 300);
+            $window.scrollTo(10, 200);
+        }
+    }, 3000);
 
     $("#header-scroll").click(function(){
-        console.log('click');
         $(window).scrollTo($("#content").position().top,500);
         location = "#content"
     });
+
+    $window.scroll(function() {
+        $nav.toggleClass('stick', $window.scrollTop() > $content.position().top);
+        console.log($window.scrollTop())
+        console.log($nav.position().top)
+    });
+
 });
